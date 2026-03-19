bd0d68dea9dadf34967e083fc8be4e54b4b94d37
gonza9514 <gonza9514@outlook.com>
2026-03-19 13:25:00 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
-window.addEventListener("scroll", function(){
+const menuToggle = document.getElementById("menuToggle");
+const menu = document.getElementById("menu");
+
+if (menuToggle && menu) {
+  menuToggle.addEventListener("click", () => {
+    menu.classList.toggle("active");
+  });
+
+  const menuLinks = menu.querySelectorAll("a");
+  menuLinks.forEach((link) => {
+    link.addEventListener("click", () => {
+      menu.classList.remove("active");
+    });
+  });
+}window.addEventListener("scroll", function(){
 
 let cards = document.querySelectorAll(".card")
 
